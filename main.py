2a30b63608c9b3f21bba10c4c31bbf0e3f1adfcf
Sean <8892151+MangoPicante@users.noreply.github.com>
2024-10-25 14:17:09 -0700

    removed column filling feature from main.py and added comments

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ if exists('book_club.json'):
     book_club = reader.json_reader('book_club.json')
 else:
     book_club = reader.csv_reader('Book Club - Ratings.csv')
-if 'goodreads' in book_club.columns:
-    mask = book_club['goodreads'].isnull()
-    book_club.loc[mask,['g_rating', 'g_genre', 'g_author', 'g_pages', 'g_published']] = book_club[mask].apply(gs.url, axis=1).apply(gs.scrape)
+    # scrapes goodreads search column for book urls
+    book_club['g_url'] = book_club.apply(gs.url, axis=1)
+    # scrapes goodreads data from urls
+    book_club[['g_rating', 'g_genre', 'g_author', 'g_pages', 'g_published']] = book_club.apply(gs.scrape)
+        
+    # scrape audible data
+    
+    # saves the data to a json file
     book_club.to_json('book_club.json', orient='records')
-else:
-    book_club[['g_rating', 'g_genre', 'g_author', 'g_pages', 'g_published']] = book_club.apply(gs.url, axis=1).apply(gs.scrape)
-    book_club.to_json('book_club.json', orient='records')
+    
+print(book_club)
