72692edc803040e523f1724e19d75b9cf12a5ba5
Sean <8892151+MangoPicante@users.noreply.github.com>
2024-11-13 20:56:54 -0800

    added user-agent to deal with goodreads 403 error

--- scraper_utils.py
+++ scraper_utils.py
@@ -4,20 +4,25 @@ import requests
 
 # Returns BeautifulSoup for the given url
 def soup_cooker(url):
+    print('Scraping:', url)
     # gets the html from the url and parses it with BeautifulSoup
-    fetched = False
-    # fetch, retrying if connection fails
-    while not fetched:
-        try:
-            html = requests.get(url, timeout=5).text
-        except Exception as e:
-            print('Connection Error', str(e))
-            return None
-        else:
-            fetched = True
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    session = requests.Session()
+    session.headers.update(headers)
+    try:
+        # fetch the HTML content from the URL
+        response = session.get(url, timeout=10)
+        # raise an exception for HTTP errors
+        response.raise_for_status()
+        html = response.text
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching the URL: {e}")
+        return None
     # parse into soup
     soup = BeautifulSoup(html, 'html.parser')
     # extracts title from soup
-    title = soup.find('title').contents[0]
-    print('Scraping:',title)
+    title = soup.find('title').text
+    print('Scraping:', title)
     return soup
