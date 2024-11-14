9db7055871cc585945e43cfe8df62b2e9f27ffcc
Sean <8892151+MangoPicante@users.noreply.github.com>
2024-11-13 19:04:21 -0800

    formatting changes

--- scraper_utils.py
+++ scraper_utils.py
@@ -4,18 +4,19 @@ import requests
 
 # Returns BeautifulSoup for the given url
 def soup_cooker(url):
+    # gets the html from the url and parses it with BeautifulSoup
     fetched = False
     # fetch, retrying if connection fails
     while not fetched:
         try:
-            response = requests.get(url)
+            html = requests.get(url, timeout=5).text
         except Exception as e:
             print('Connection Error', str(e))
             return None
         else:
             fetched = True
     # parse into soup
-    soup = BeautifulSoup(response.text, 'html.parser')
+    soup = BeautifulSoup(html, 'html.parser')
     # extracts title from soup
     title = soup.find('title').contents[0]
     print('Scraping:',title)
