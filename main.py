a2c48ee7d5be6c2cc37e3777a3fa048701754fe7
Sean <8892151+MangoPicante@users.noreply.github.com>
2024-11-14 12:11:12 -0800

    added csv export feature

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ def json_save_prompt(df, filename):
     if prompt == 'y':
         df.to_json(filename + '.json', orient='records')
 
+def csv_save_prompt(df, filename):
+    # saves the data to a csv file if prompt accepted
+    prompt = input('Save data to csv? (y/n): ')
+    if prompt == 'y':
+        df.to_csv(filename + '.csv', index=False)
+
 def column_backfill(df, columns, scraper):
     if not all(column in df.columns for column in columns):
         print('Columns {columns} not found in dataframe')
@@ -33,11 +39,12 @@ else:
         book_club = reader.csv_reader('Book Club - Ratings.csv')
         print(book_club.head())
         # scrapes goodreads data from urls
-        book_club[['g_rating', 'g_genre', 'g_author', 'g_pages', 'g_published']] = book_club.apply(gscr.scrape)
+        book_club[['g_rating', 'g_genre', 'g_author', 'g_pages', 'g_published']] = book_club.apply(gscr.scrape, axis=1)
         json_save_prompt(book_club, 'book_club')
         # scrapes audible data from urls
-        book_club[['a_ratings', 'a_runtime', 'a_author', 'a_narrator', 'a_categories', 'a_tags']] = book_club.apply(ascr.scrape)
+        book_club[['a_ratings', 'a_runtime', 'a_author', 'a_narrator', 'a_categories', 'a_tags']] = book_club.apply(ascr.scrape, axis=1)
         json_save_prompt(book_club, 'book_club')
     except Exception as e:
         print(f"Error: {e}")
-    
+
+csv_save_prompt(book_club, 'book_club')
